a63adbabff8f2a28274ceabef7526ecda82aa305
ahron <asmillrod@gmail.com>
2024-06-24 14:23:49 -0400

    staege

---
+++ Account.php
@@ -0,0 +1,51 @@
+<?php
+require_once(APPPATH . 'views/Header.php');
+?>
+
+
+<div class="account" >
+
+    <h5 class="account_login" style="font-family: Roboto Slab, serif;">LOGIN PAGE</h5>
+
+    <div class="account-fileds">
+          <input type="email" placeholder="Email" name="email" class="form-control account-text" >
+          <input type="password" placeholder="Password" name="password" class="form-control account-text" >
+          <button class="login_btn" >Log In</button>
+          <p class="signup" >Don't have an account? <a class="signup" href="signup">Sign Up</a></p>
+        </div>
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<hr>
+
+<?php
+require_once(APPPATH . 'views/footer.php');
+?>
